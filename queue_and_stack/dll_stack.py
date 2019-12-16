6a15693215cf3f1cd183f1345cb0ee40c382511d
Seth Nadu <seth.nadu@gmail.com>
2019-12-16 14:50:06 -0500

    make the queue_and_stack files match the moved queue and stack files in doubly_linked_list, had import problems

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -3,18 +3,19 @@ sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
 class Stack:
+    # Can only push and pop to end of stack!
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
         self.storage = DoublyLinkedList()
 
     def push(self, value):
-        while self.storage.head:
-            self.storage.add_to_head(value)
+        return self.storage.add_to_tail(value)
 
     def pop(self):
-        while self.storage.head:
-            self.storage.remove_from_head()
+        while self.storage.length > 0:
+            return self.storage.remove_from_tail()
 
     def len(self):
         return self.storage.length
+
